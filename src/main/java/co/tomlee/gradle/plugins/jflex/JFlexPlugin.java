1bbdf2d6dbf702944d11e24a859e0fe4f957032f
Tom Lee <github@tomlee.co>
2016-04-15 00:03:38 -0700

    Fix stuff for Gradle 2.12

--- src/main/java/co/tomlee/gradle/plugins/jflex/JFlexPlugin.java
+++ src/main/java/co/tomlee/gradle/plugins/jflex/JFlexPlugin.java
@@ -7,6 +7,7 @@ import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.internal.file.FileResolver;
 import org.gradle.api.internal.plugins.DslObject;
 import org.gradle.api.internal.tasks.DefaultSourceSet;
+import org.gradle.api.internal.file.collections.DirectoryFileTreeFactory;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
@@ -17,10 +18,12 @@ import java.util.concurrent.Callable;
 
 public final class JFlexPlugin implements Plugin<Project> {
     private final FileResolver fileResolver;
+    private final DirectoryFileTreeFactory directoryFileTreeFactory;
 
     @Inject
-    public JFlexPlugin(final FileResolver fileResolver) {
+    public JFlexPlugin(final FileResolver fileResolver, final DirectoryFileTreeFactory directoryFileTreeFactory) {
         this.fileResolver = fileResolver;
+        this.directoryFileTreeFactory = directoryFileTreeFactory;
     }
 
     @Override
@@ -44,7 +47,10 @@ public final class JFlexPlugin implements Plugin<Project> {
                 // 1. Add a new 'jflex' virtual directory mapping
                 //
                 final JFlexVirtualSourceDirectoryImpl jflexSourceSet =
-                    new JFlexVirtualSourceDirectoryImpl(((DefaultSourceSet) sourceSet).getDisplayName(), fileResolver);
+                    new JFlexVirtualSourceDirectoryImpl(
+                            ((DefaultSourceSet) sourceSet).getDisplayName(),
+                            fileResolver,
+                            directoryFileTreeFactory);
                 new DslObject(sourceSet).getConvention().getPlugins().put("jflex", jflexSourceSet);
                 final String srcDir = String.format("src/%s/jflex", sourceSet.getName());
                 jflexSourceSet.getJflex().srcDir(srcDir);
--- src/main/java/co/tomlee/gradle/plugins/jflex/JFlexTask.java
+++ src/main/java/co/tomlee/gradle/plugins/jflex/JFlexTask.java
@@ -3,22 +3,41 @@ package co.tomlee.gradle.plugins.jflex;
 import groovy.lang.Closure;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.SourceTask;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.artifacts.ResolvedConfiguration;
+import org.gradle.api.artifacts.ResolvedArtifact;
 import org.gradle.process.JavaExecSpec;
 
 import java.io.File;
+import java.util.Set;
 
 public class JFlexTask extends SourceTask {
     private FileCollection jflexClasspath;
     private File outputDirectory;
 
+    private String mainClass = "jflex.Main";
+
+    public void setMainClass(final String mainClass) {
+        this.mainClass = mainClass;
+    }
+
+    public void mainClass(final String mainClass) {
+        this.mainClass = mainClass;
+    }
+
+    @Input
+    public String getMainClass() {
+        return mainClass;
+    }
+
     @TaskAction
     public void generate() throws Exception {
         getProject().javaexec(new Closure(this) {
             public void doCall(JavaExecSpec javaExecSpec) {
-                javaExecSpec.setMain("jflex.Main")
+                javaExecSpec.setMain(mainClass)
                         .setClasspath(getProject().getConfigurations().getByName("jflex"))
                         .args("-d")
                         .args(getOutputDirectory())
